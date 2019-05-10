acbff8821bf98ef1d3564852c7342f848c5f3dae
AmiT177 <8685831+AmiT177@users.noreply.github.com>
2019-05-10 07:48:57 +0300

    Update copyright year

--- src/clientversion.h
+++ src/clientversion.h
@@ -27,6 +27,7 @@
  * Todo: update this when changing our copyright comments in the source
  */
 #define COPYRIGHT_YEAR 2017
+#define HODL_COPYRIGHT_YEAR 2019
 
 #endif //HAVE_CONFIG_H
 
@@ -38,7 +39,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The HOdlcoin Developers"
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2017-" STRINGIZE(HODL_COPYRIGHT_YEAR) " The HOdlcoin Developers"
 
 /**
  * bitcoind-res.rc includes this file, but it cannot cope with real c++ code.
