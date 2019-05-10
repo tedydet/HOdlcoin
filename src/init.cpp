127e05f918155b8f1054e5a06425c68d0fc75752
AmiT177 <8685831+AmiT177@users.noreply.github.com>
2019-05-10 11:25:50 +0300

    Update copyright year

--- src/init.cpp
+++ src/init.cpp
@@ -452,7 +452,7 @@ std::string LicenseInfo()
 {
     return FormatParagraph(strprintf(_("Copyright (C) 2009-%i The Bitcoin Core Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
-           FormatParagraph(strprintf(_("Copyright (C) 2015-%i The HOdlcoin Developers"), HODL_COPYRIGHT_YEAR)) + "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2017-%i The HOdlcoin Developers"), HODL_COPYRIGHT_YEAR)) + "\n" +
            "\n" +
            FormatParagraph(_("This is experimental software.")) + "\n" +
            "\n" +
